39b1bd7ceca9b10c6eb14c196faa40ca0fe5414a
Chilly <nitro0323@gmail.com>
2022-09-04 17:54:31 +0400

    L2- Task_10 complete

---
+++ Lesson2/Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите 3х значное число");
+string inputString = Console.ReadLine();
+if(inputString.Length != 3)
+{
+    Console.WriteLine("Неправильное количество символов");
+}
+else
+{
+    int Number = int.Parse(inputString);
+    Console.WriteLine("Вторая цифра:" + (Number/10)%10);
+    //Console.WriteLine("Вторая цифра:" + inputString[1]);
+    //формально с числом я так и не поработал. Так что решил сделать оба варианта.
+}
