df9ab543287a35f0524e6eb480bc53140d0eb888
Chilly <nitro0323@gmail.com>
2022-09-04 18:13:59 +0400

    Task_13 & Task_15 comple

---
+++ Lesson2/Task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+int Number = new Random().Next(0,9999);
+Console.WriteLine(Number);
+string dumbNaming = Convert.ToString(Number);
+if (dumbNaming.Length>2)
+{
+    Console.WriteLine(dumbNaming[2] + " - наша третья цифра");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
---
+++ Lesson2/Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int dayOfWeek = int.Parse(Console.ReadLine());
+if (dayOfWeek<8 && dayOfWeek>0)
+{
+    if(dayOfWeek==6 || dayOfWeek==7)
+    {
+        Console.WriteLine("Выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Будний((");
+    }
+}
+else
+{
+    Console.WriteLine("Число выходит за диапазон [1;7]");
+}
